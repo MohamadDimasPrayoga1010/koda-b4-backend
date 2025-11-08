e519285736b45b13f4518f7c94522cef78caaf3c
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-09 02:10:00 +0700

    feat: create primary calling routers

--- routers/router.go
+++ routers/router.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+
 	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -8,5 +9,6 @@ import (
 func InitRouter (pg *pgxpool.Pool) *gin.Engine{
 	r := gin.Default()
 
+	AuthRoutes(r, pg)
 	return r
 }
