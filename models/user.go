73e73886f425218cbec2c22887a3ee8bb4f457a0
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-09 02:08:23 +0700

    feat: create user models

---
+++ models/user.go
@@ -0,0 +1,21 @@
+package models
+
+import "time"
+
+type User struct{
+	ID        int64     `json:"id" db:"id"`
+	Fullname  string    `json:"fullname" db:"fullname"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"-" db:"password"`
+	Role      string    `json:"role" db:"role"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type UserResponse struct {
+	ID       int64  `json:"id"`
+	Fullname string `json:"fullname"`
+	Email    string `json:"email"`
+	Role     string `json:"role"`
+	Token    string `json:"token,omitempty"`
+}
