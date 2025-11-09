6bb8a44793447f9b14101cf37e13dc250a29b0e1
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-10 00:54:56 +0700

    feat: create a router for product admin

---
+++ routers/product.router.go
@@ -0,0 +1,21 @@
+package routers
+
+import (
+	"main/controllers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func ProductRoutes(r *gin.Engine, pg *pgxpool.Pool) {
+	pc := controllers.ProductController{DB: pg}
+
+	admin := r.Group("/admin")
+	{
+		admin.POST("/products", pc.CreateProduct)
+		admin.GET("/products", pc.GetProduct)
+		admin.GET("/products/:id", pc.GetProductByID)
+		admin.PATCH("/products/:id", pc.UpdateProduct)
+		admin.DELETE("/products/:id", pc.DeleteProduct)
+	}
+}
