884a6cbd8d785d7c9c5f607681a564421792ccfe
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-10 00:52:48 +0700

    refactor: fix user struct on password

--- models/user.go
+++ models/user.go
@@ -6,7 +6,7 @@ type User struct{
 	ID        int64     `json:"id" db:"id"`
 	Fullname  string    `json:"fullname" db:"fullname"`
 	Email     string    `json:"email" db:"email"`
-	Password  string    `json:"-" db:"password"`
+	Password  string    `json:"password" db:"password"`
 	Role      string    `json:"role" db:"role"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
