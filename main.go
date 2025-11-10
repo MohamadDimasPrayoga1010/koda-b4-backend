d4db3b10d789234db74aedf745d29a23a40512d2
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-10 08:37:02 +0700

    feat: add swager documentation

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+
 	"main/configs"
+	"main/docs"
 	"main/routers"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Coffee Shop API
+// @version 1.0
+// @description API for Coffee Shop Application
+// @host localhost:8085
+// @BasePath /
 func main() {
 	pg := configs.InitDbConfig()
 	r := routers.InitRouter(pg)
-	
+
+	docs.SwaggerInfo.BasePath = "/"
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	// fmt.Println(libs.HashPassword("admin123"))
 	r.Run(":8085")
 }
