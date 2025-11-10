835613e7c2f7377ba72c2cf18b0477988729ff89
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-10 21:28:10 +0700

    refactor: fix the struct order

--- models/order.go
+++ models/order.go
@@ -2,23 +2,22 @@ package models
 
 import "time"
 
-type Order struct {
-	ID               int64           `json:"id"`
-	NoOrders         string          `json:"no_orders"`
-	Total            float64         `json:"total"`
-	StatusID         int64           `json:"status_id"`
-	StatusName       string          `json:"status_name"`
-	UserID           int64           `json:"user_id"`
-	UserName         string          `json:"user_name"`
-	PaymentMethodID  int64           `json:"payment_method_id"`
-	PaymentMethod    string          `json:"payment_method"`
-	ShippingName     string          `json:"shipping_name"`
-	CreatedAt        time.Time       `json:"created_at"`
-	OrderItems       []OrderItemResp `json:"order_items,omitempty"`
+type TransactionItem struct {
+	Title string  `json:"title"`
+	Qty   int     `json:"qty"`
+	Size  string  `json:"size,omitempty"` 
 }
 
-type OrderItemResp struct {
-	Title string `json:"title"`
-	Size  string `json:"size"`
-	Qty   int    `json:"qty"`
+type Transaction struct {
+	ID            int64             `json:"id"`
+	NoOrders      string            `json:"no_orders"`
+	CreatedAt     time.Time         `json:"created_at"`
+	StatusName    string            `json:"status_name"`
+	Total         float64           `json:"total"`
+	UserFullname  string            `json:"user_fullname"`
+	UserAddress   string            `json:"user_address"`
+	UserPhone     string            `json:"user_phone"`
+	PaymentMethod string            `json:"payment_method"`
+	ShippingName  string            `json:"shipping_name"`
+	OrderItems    []TransactionItem `json:"order_items"`
 }
