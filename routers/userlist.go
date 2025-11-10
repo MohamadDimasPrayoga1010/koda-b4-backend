207909300addf7d3b2ed1b740338074f85389c41
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-10 08:35:09 +0700

    feat: create routers for userlist

---
+++ routers/userlist.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	"main/controllers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func AdminUserRoutes(r *gin.Engine, pg *pgxpool.Pool) {
+	uc := controllers.UserController{DB: pg}
+
+	admin := r.Group("/admin")
+	{
+		admin.GET("/userslist", uc.GetUsersList)      
+		admin.POST("/userslist", uc.AddUser)          
+		admin.PATCH("/userslist/:id", uc.EditUser)      
+	}
+}
