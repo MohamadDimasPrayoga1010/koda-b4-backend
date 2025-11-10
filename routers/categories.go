5e256f05be7ce079ca4ac342d9fe757077cb6bec
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-10 21:44:55 +0700

    feat: create routers for categories

---
+++ routers/categories.go
@@ -0,0 +1,21 @@
+package routers
+
+import (
+	"main/controllers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CategoryRoutes(r *gin.Engine, pg *pgxpool.Pool) {
+	cc := controllers.CategoryController{DB: pg}
+
+	admin := r.Group("/admin")
+	{
+		admin.GET("/categories", cc.GetCategories)
+		admin.GET("/categories/:id", cc.GetCategoryByID)
+		admin.POST("/categories", cc.CreateCategory)
+		admin.PATCH("/categories/:id", cc.UpdateCategory)
+		admin.DELETE("/categories/:id", cc.DeleteCategory)
+	}
+}
