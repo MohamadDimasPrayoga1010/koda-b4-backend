f2f47b0b0630fa596ee203438524d102780edffc
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-10 21:23:11 +0700

    feat: add struct forgot password, verify and also reset password

--- models/auth.go
+++ models/auth.go
@@ -11,3 +11,17 @@ type UserLogin struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
 }
+
+type ForgotPasswordRequest struct {
+	Email string `json:"email" binding:"required,email"`
+}
+
+type VerifyOTPRequest struct {
+	Email string `json:"email" binding:"required,email"`
+	OTP   string `json:"otp" binding:"required,len=6"`
+}
+
+type ResetPasswordRequest struct {
+	Token    string `json:"token" binding:"required"`
+	Password string `json:"password" binding:"required,min=6"`
+}
