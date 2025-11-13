2646de40b42b477172109b3a546a29bb00c04b5f
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-13 16:38:39 +0700

    feat: change the default progress

--- models/product.go
+++ models/product.go
@@ -468,7 +468,7 @@ func CreateOrderTransaction(db *pgxpool.Pool, req OrderTransactionRequest) (*Ord
 		ShippingName:      shippingName,
 		InvoiceNumber:     invoice,
 		Total:             total,
-		Status:            "Pending",
+		Status:            "OnProgress",
 		CreatedAt:         createdAt,
 		UpdatedAt:         updatedAt,
 		Items:             items,
