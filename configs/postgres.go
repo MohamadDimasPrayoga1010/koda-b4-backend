8d73083f697dc4d2276c59b7fd458c57c665086b
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-13 16:39:23 +0700

    refactor: fix database env

--- configs/postgres.go
+++ configs/postgres.go
@@ -2,7 +2,6 @@ package configs
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"time"
@@ -12,19 +11,12 @@ import (
 )
 
 func InitDB() *pgxpool.Pool {
+	
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Failed to load .env")
 	}
 
-	connStr := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-	)
-
+	connStr := os.Getenv("DATABASE_URL")
 	pool, err := pgxpool.New(context.Background(), connStr)
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
