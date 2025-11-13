fd1d377e0e4b849b54f08c1736e7d49535f3108d
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-13 20:36:18 +0700

    set up deploy

--- api/main.go
+++ api/main.go
@@ -15,14 +15,17 @@ import (
 var router *gin.Engine
 
 func init() {
-	pg := configs.InitDbConfig()
-	libs.InitRedis()
-	router = routers.InitRouter(pg)
 
 	docs.SwaggerInfo.BasePath = "/"
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	if router == nil {
+		pg := configs.InitDbConfig()
+		libs.InitRedis()
+		router = routers.InitRouter(pg)
+	}
 	router.ServeHTTP(w, r)
 }
+
