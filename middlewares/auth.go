cd60956ef005e6bd2d112ca3e3403e97af3e2653
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-15 01:04:53 +0700

    refactor: fix auth middleware

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,60 +1,18 @@
 package middlewares
 
 import (
-	"fmt"
-	"main/libs"
 	"os"
 	"strings"
+
+	"main/libs"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
-func AuthRequired() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		userID, exists := ctx.Get("userID")
-		if !exists {
-			ctx.JSON(401, gin.H{
-				"success": false,
-				"message": "Unauthorized: missing user info",
-			})
-			ctx.Abort()
-			return
-		}
-		ctx.Set("userID", userID)
-		ctx.Next()
-	}
-}
-
-func AdminOnly() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		roleValue, exists := ctx.Get("userRole")
-		fmt.Println(roleValue,"gagal")
-		if !exists {
-			ctx.JSON(401, gin.H{
-				"success": false,
-				"message": "Unauthorized: missing role",
-			})
-			ctx.Abort()
-			return
-		}
-
-		role, ok := roleValue.(string)
-		fmt.Println(role,"gagal")
-		if !ok || role != "admin" {
-			ctx.JSON(403, gin.H{
-				"success": false,
-				"message": "Forbidden: admin access only",
-			})
-			ctx.Abort()
-			return
-		}
-
-		ctx.Next()
-	}
-}
 
 func AuthMiddleware(requiredRole string) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+
 		authHeader := ctx.GetHeader("Authorization")
 		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
 			ctx.JSON(401, gin.H{"success": false, "message": "Missing or invalid Authorization header"})
@@ -63,9 +21,15 @@ func AuthMiddleware(requiredRole string) gin.HandlerFunc {
 		}
 
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-		secret := os.Getenv("JWT_SECRET")
-		claims := &libs.UserPayload{}
 
+		secret := os.Getenv("JWT_SECRET")
+		if secret == "" {
+			ctx.JSON(500, gin.H{"success": false, "message": "Server missing JWT secret"})
+			ctx.Abort()
+			return
+		}
+
+		claims := &libs.UserPayload{}
 		token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
 			return []byte(secret), nil
 		})
@@ -76,10 +40,9 @@ func AuthMiddleware(requiredRole string) gin.HandlerFunc {
 			return
 		}
 
-		ctx.Set("userID", int64(claims.Id))  
+		ctx.Set("userID", claims.Id)
 		ctx.Set("userEmail", claims.Email)
 		ctx.Set("userRole", claims.Role)
-
 
 		if requiredRole != "" && claims.Role != requiredRole {
 			ctx.JSON(403, gin.H{"success": false, "message": "Not permission"})
@@ -90,5 +53,3 @@ func AuthMiddleware(requiredRole string) gin.HandlerFunc {
 		ctx.Next()
 	}
 }
-
- 
