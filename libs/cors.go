a19439ccb8a8758010266d95476da83f9126a751
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-20 10:54:16 +0700

    fix cors

--- libs/cors.go
+++ libs/cors.go
@@ -2,7 +2,6 @@ package libs
 
 import (
 	"os"
-	"strings"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -10,26 +9,14 @@ import (
 )
 
 func SetupCORS() gin.HandlerFunc {
-	envOrigins := os.Getenv("ALLOW_ORIGIN")
-	origins := []string{"http://localhost:5173"} 
-
-	if envOrigins != "" {
-		origins = append(origins, strings.Split(envOrigins, ",")...)
-	}
-
+	origin := os.Getenv("ALLOW_ORIGIN")
+	
 	config := cors.Config{
+		AllowOrigins:     []string{origin},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
 		MaxAge:           24 * time.Hour,
-		AllowOriginFunc: func(origin string) bool {
-			for _, o := range origins {
-				if o == origin {
-					return true
-				}
-			}
-			return false
-		},
 	}
 
 	return cors.New(config)
